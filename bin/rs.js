46eec3328b8976530f5543dfad425da10f64c518
neverland <chenjiahan.jait@bytedance.com>
2026-06-29 21:48:23 +0800

    feat: setup bin file

---
+++ bin/rs.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import nodeModule from 'node:module';
+
+// enable on-disk code caching of all modules loaded by Node.js
+// requires Nodejs >= 22.8.0
+const { enableCompileCache } = nodeModule;
+if (enableCompileCache) {
+  try {
+    enableCompileCache();
+  } catch {
+    // ignore errors
+  }
+}
