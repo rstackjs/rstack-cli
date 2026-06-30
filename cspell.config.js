cbbe1a65197eda94259e429b496c2241852f4c6b
neverland <chenjiahan.jait@bytedance.com>
2026-06-30 11:30:35 +0800

    chore: setup cspell

---
+++ cspell.config.js
@@ -0,0 +1,33 @@
+import { banWords } from 'cspell-ban-words';
+
+export default {
+  version: '0.2',
+  language: 'en',
+  files: ['**/*.{ts,tsx,js,jsx,md,mdx}'],
+  enableFiletypes: ['mdx'],
+  ignoreRegExpList: [
+    // ignore markdown anchors such as [modifyRsbuildConfig](#modifyrsbuildconfig)
+    '#.*?\\)',
+    // ignore custom anchor declarations such as ## createRsbuild \{#creatersbuild}
+    '\\\\\\{#[^}]+\\}',
+    // ignore release note author mentions such as "by @chenjiahan"
+    'by\\s+@[A-Za-z0-9_-]+',
+  ],
+  ignorePaths: [
+    'dist',
+    'dist-*',
+    'coverage',
+    'doc_build',
+    'node_modules',
+    'pnpm-lock.yaml',
+  ],
+  flagWords: banWords,
+  dictionaries: ['dictionary'],
+  dictionaryDefinitions: [
+    {
+      name: 'dictionary',
+      path: './scripts/dictionary.txt',
+      addWords: true,
+    },
+  ],
+};
