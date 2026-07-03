04ad27716d99dd2c267a009d88f8caae032cb0e0
neverland <chenjiahan.jait@bytedance.com>
2026-07-03 11:28:24 +0800

    docs: add JSDoc for rstack/test

--- packages/rstack/src/test.ts
+++ packages/rstack/src/test.ts
@@ -1,1 +1,9 @@
+/**
+ * Re-export @rstest/core APIs so users can import test APIs from `rstack/test`.
+ *
+ * @example
+ * ```ts
+ * import { expect, test } from 'rstack/test';
+ * ```
+ */
 export * from '@rstest/core';
