e586d17207be75cdb9913078b0c6cddfe2b86f32
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-07-12 13:20:07 +0800

    test: speed up staged CLI tests (#35)

--- packages/rstack/rstack.config.ts
+++ packages/rstack/rstack.config.ts
@@ -1,10 +1,13 @@
 import { define } from 'rstack';
 
-define.test(() => {
+define.test(async () => {
+  const { withRslibConfig } = await import('@rstest/adapter-rslib');
+
   // Disable color in test
   process.env.NO_COLOR = '1';
 
   return {
+    extends: withRslibConfig(),
     source: {
       tsconfigPath: './test/tsconfig.json',
     },
