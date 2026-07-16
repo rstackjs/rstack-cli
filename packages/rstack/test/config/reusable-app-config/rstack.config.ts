f0e292b6241cc7fd1e9f983274ec81fbd88f843d
neverland <chenjiahan.jait@bytedance.com>
2026-07-16 21:42:47 +0800

    test: remove reusable config regression coverage

--- packages/rstack/test/config/reusable-app-config/rstack.config.ts
+++
@@ -1,9 +0,0 @@
-import type { RsbuildConfig } from '@rsbuild/core';
-import { define } from 'rstack';
-
-declare global {
-  // rslint-disable-next-line no-var
-  var __rstackReusableAppConfig: RsbuildConfig | undefined;
-}
-
-define.app(() => globalThis.__rstackReusableAppConfig ?? {});
