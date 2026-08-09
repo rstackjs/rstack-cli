dc3be1a848a28f4311f06f6fe8692c697bd46e88
neverland <chenjiahan.jait@bytedance.com>
2026-08-09 21:29:20 +0800

    fix(create-rstack): auto unmount Vue wrappers in templates

--- packages/create-rstack/template-lib-vue-ts/tests/rstest.setup.ts
+++ packages/create-rstack/template-lib-vue-ts/tests/rstest.setup.ts
@@ -1,4 +1,6 @@
-import { expect } from 'rstack/test';
+import { afterEach, expect } from 'rstack/test';
 import * as jestDomMatchers from '@testing-library/jest-dom/matchers';
+import { enableAutoUnmount } from '@vue/test-utils';
 
 expect.extend(jestDomMatchers);
+enableAutoUnmount(afterEach);
